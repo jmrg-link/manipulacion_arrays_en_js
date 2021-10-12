4a603cc97c8bf06d9c3f3973d3699c76d040cc8d
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-13 01:34:15 +0200

    flatmap example

--- server/flatmap.js
+++ server/flatmap.js
@@ -1,7 +1,43 @@
 const users = [
-    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
-    { userId: 2, username: "Mike", attributes: ["Lovely"]},
-    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
-  ]
+  { userId: 1, username: "Tom", attributes: ["Nice", "Cute"] },
+  { userId: 2, username: "Mike", attributes: ["Lovely"] },
+  { userId: 3, username: "Nico", attributes: ["Nice", "Cool"] },
+];
 
-  
+const rta = users.map((user) => user.attributes).flat();
+console.log("flat-map", rta);
+const rta2 = users.flatMap((user) => user.attributes);
+console.log("flatmap", rta2);
+
+const calendars = {
+  primaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "Cita 1",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 17),
+      endDate: new Date(2021, 1, 1, 18),
+      title: "Cita 2",
+    },
+  ],
+  secondaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 12),
+      endDate: new Date(2021, 1, 1, 12, 30),
+      title: "Cita 2",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 9),
+      endDate: new Date(2021, 1, 1, 10),
+      title: "Cita 4",
+    },
+  ],
+};
+
+const rta3 = Object.values(calendars).flatMap((item) => {
+  console.log("item:", item);
+  return item.map((date) => date.startDate);
+});
+console.log("calendars google: ", rta3);
