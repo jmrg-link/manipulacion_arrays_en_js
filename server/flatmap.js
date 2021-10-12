c38680422452d7aeb08866e5be41f4965e83b0bd
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-13 00:16:31 +0200

    flatmap

---
+++ server/flatmap.js
@@ -0,0 +1,7 @@
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
+    { userId: 2, username: "Mike", attributes: ["Lovely"]},
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
+  ]
+
+  
