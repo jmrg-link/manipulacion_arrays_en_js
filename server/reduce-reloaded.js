d8259f36e9c4b39c36b3cc89c86afa584a9b267a
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-12 17:18:07 +0200

    reduce-reloaded

--- server/reduce-reloaded.js
+++ server/reduce-reloaded.js
@@ -21,3 +21,33 @@ const items = [1,3,2,3]
 }
 */
 
+const data = [
+    {
+        name:"Jesus",
+        lvl:"low"
+    },
+    {
+        name:"Lourdes",
+        lvl:"medium"
+    },
+    {
+        name:"Jaime",
+        lvl:"hight"
+    },
+    {
+        name:"Xabi",
+        lvl:"low"
+    },
+]
+const result2 = data
+    .map(item => item.lvl)
+    .reduce((obj,item)=>{
+        if (!obj[item]) {
+            obj[item] = 1
+        }else {
+            obj[item] = obj[item] + 1
+        }
+        return obj
+    },{})
+
+    console.log('result2: ', result2);
