4fa028a9946e633385c668edfb4fe56675c402ad
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-12 23:00:17 +0200

    find & findIndex

--- server/find.js
+++ server/find.js
@@ -1,6 +1,6 @@
 const numbers = [1,30,49,29,10,13]
 
-let rta = null
+let rta = undefined
 
 for (let index = 0; index < numbers.length; index++) {
     const element = numbers[index];
@@ -11,3 +11,34 @@ for (let index = 0; index < numbers.length; index++) {
 }
 
 console.log('for : ', rta);
+const rta2 = numbers.find(item => item === 30)
+console.log('find : ' , rta2);
+
+
+const products = [
+    {
+      name: "Pizza",
+      price: 12,
+      id: '🍕'
+    },
+    {
+      name: "Burger",
+      price: 23,
+      id: '🍔'
+    },
+    {
+      name: "Hot dog",
+      price: 34,
+      id: '🌭'
+    },
+    {
+      name: "Hot cakes",
+      price: 355,
+      id: '🥞'
+    },
+  ];
+  const rta3 = products.find(item => item['id'] === '🍔' )
+  console.log('find : ' , rta3);
+  
+  const rta4 = products.findIndex(item => item['id'] === '🍔' )
+  console.log('findIndex : ' , rta4);
