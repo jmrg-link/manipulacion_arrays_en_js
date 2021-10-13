724f36c7a3e88c1eb96ed35dd6d9b49082923d7e
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2021-10-13 02:11:03 +0200

    mutables

--- server/mutables.js
+++ server/mutables.js
@@ -5,10 +5,43 @@ const products = [
     { name: "Hot cakes", price: 121, id: '🥞' },
 ];
 const myProducts = []
+console.group('Reto-1')
 console.log("Products:",products);
 console.log("MyProducts:", myProducts);
-console.log("newArray:",newArray);
 console.log("-".repeat(10));
+console.groupEnd('Reto-1')
 
+const productIndex = products.findIndex(item => item.id === '🍔')
+if(productIndex !== -1){
+    myProducts.push(products[productIndex])
+    products.splice(2,1)
+}
+
+console.group('Reto-1.1')
+console.log("Products:",products);
+console.log("MyProducts:", myProducts);
+console.log("-".repeat(10));
+console.groupEnd('Reto-1.1')
 
 //Reto:2
+const productsV2 = [
+    {title: "Pizza", price: 121, id: "🍕"},  
+    { name: "Burger", price: 121, id: '🍔' },
+    { name: "Hot cakes", price: 121, id: '🥞' },
+];
+
+const update = {
+    id:'🥞',
+    changes: {
+        price: 200,
+        description: 'delicioso'
+    }
+}
+
+const productIndexV2 = productsV2.findIndex(item => item.id === update.id)
+productsV2[productIndexV2] = {
+    ...productsV2[productIndexV2],
+    ...update.changes
+}
+
+console.log('productsV2', productsV2);
